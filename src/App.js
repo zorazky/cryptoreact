2a71a518eb239fde42487d2c18b0751e954c9b5d
Oswaldo <oswaldo.vazquez@gmail.com>
2018-11-13 10:32:59 -0600

    Header created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Header from './componentes/Header';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       <h1>Crypto</h1>
+      <div className="container">
+        <Header titulo="Cotiza Criptomonedas al instante" />
       </div>
     );
   }
